cb58e561ebe88e48e0d00179859caf12dde61dcd
mithem <mithem@github.com>
2021-10-08 15:51:00 +0200

    Initial Commit

---
+++ GLapp/GLappApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLappApp.swift
+//  GLapp
+//
+//  Created by Miguel Themann on 08.10.21.
+//
+
+import SwiftUI
+
+@main
+struct GLappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
